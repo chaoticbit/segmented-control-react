27355310b0249156fff5131a670e7d85fde34559
Atharva Dandekar <adandeka@nyit.edu>
2018-08-20 09:24:03 -0700

    Segment disable feature (#2)
    * Added .idea & .vscode in gitignore
    
    * Feature: add disable to specific segment
    
    * Added src folder to .npmignore
    
    * Refactored

--- dist/bundle.js
+++ dist/bundle.js
@@ -35,7 +35,7 @@ function styleInject(css, ref) {
   }
 }
 
-var css = ".r-segmented-control {\n    width: 100%;\n    background-color: #fff;    \n    border-bottom: 1px solid #e9eff3;\n    border-left: 1px solid #fff;\n    text-align: center;\n    height: 38px;\n    -webkit-box-sizing: border-box;\n    box-sizing: border-box;\n    direction: ltr;\n    display: table;\n}\n\n.r-segmented-control > ul {\n    display: table-row;\n    display: -webkit-box;\n    display: -ms-flexbox;\n    display: flex;\n    padding: 6px 8px;\n}\n\n.r-segmented-control > ul > li {\n    background: #fff;\n    border: 1px solid;    \n    border-right: none;\n    font-size: 13px;\n    height: 26px;\n    cursor: pointer;\n    -webkit-user-select: none;\n    -moz-user-select: none;\n    -ms-user-select: none;\n    user-select: none;\n    -webkit-box-sizing: border-box;\n    box-sizing: border-box;\n    vertical-align: middle;\n    display: table-cell;\n    display: -webkit-box;\n    display: -ms-flexbox;\n    display: flex;\n    align-items: center;\n    justify-content: center;\n    -ms-flex: auto;\n    flex: auto;\n}\n\n.r-segmented-control > ul > li:first-of-type {\n    border-top-left-radius: 4px;\n    border-bottom-left-radius: 4px;\n}\n\n.r-segmented-control > ul > li:last-of-type {\n    border-right: 1px solid;\n    border-top-right-radius: 4px;\n    border-bottom-right-radius: 4px;\n}\n\n.r-segmented-control > ul > li.base {\n    border-color: rgb(0,122,255) !important;\n    color: rgb(0,122,255);\n    background: #fff !important;\n}\n\n.r-segmented-control > ul > li.dark {\n    border-color: #333 !important;\n    color: #333;\n    background: #fff !important;\n}\n\n.r-segmented-control > ul > li.light {\n    border-color: rgb(247, 247, 247) !important;\n    color: rgb(247, 247, 247);\n    background: transparent !important;\n}\n\n.r-segmented-control > ul > li.success {\n    border-color: #5cb85c !important;\n    color: #5cb85c;\n    background: #fff !important;\n}\n\n.r-segmented-control > ul > li.error {\n    border-color: #d9534f !important;\n    color: #d9534f;\n    background: #fff !important;\n}\n\n.r-segmented-control > ul > li.base.selected {\n    background: rgb(0,122,255) !important;    \n    color: #fff !important;\n}\n\n.r-segmented-control > ul > li.dark.selected {\n    background: #333 !important;\n    color: #fff !important;\n}\n\n.r-segmented-control > ul > li.light.selected {\n    background: rgb(247, 247, 247) !important;\n    color: #333 !important;\n}\n\n.r-segmented-control > ul > li.success.selected {\n    background: #5cb85c !important; \n    color: #fff !important;\n}\n\n.r-segmented-control > ul > li.error.selected {\n    background: #d9534f !important;\n    color: #fff !important;\n}";
+var css = ".r-segmented-control {\n    width: 100%;\n    background-color: #fff;    \n    border-bottom: 1px solid #e9eff3;\n    border-left: 1px solid #fff;\n    text-align: center;\n    height: 38px;\n    -webkit-box-sizing: border-box;\n    box-sizing: border-box;\n    direction: ltr;\n    display: table;\n}\n\n.r-segmented-control > ul {\n    display: table-row;\n    display: -webkit-box;\n    display: -ms-flexbox;\n    display: flex;\n    padding: 6px 8px;\n}\n\n.r-segmented-control > ul > li {\n    background: #fff;\n    border: 1px solid;    \n    border-right: none;\n    font-size: 13px;\n    height: 26px;\n    cursor: pointer;\n    -webkit-user-select: none;\n    -moz-user-select: none;\n    -ms-user-select: none;\n    user-select: none;\n    -webkit-box-sizing: border-box;\n    box-sizing: border-box;\n    vertical-align: middle;\n    display: table-cell;\n    display: -webkit-box;\n    display: -ms-flexbox;\n    display: flex;\n    align-items: center;\n    justify-content: center;\n    -ms-flex: auto;\n    flex: auto;\n}\n\n.r-segmented-control > ul > li:first-of-type {\n    border-top-left-radius: 4px;\n    border-bottom-left-radius: 4px;\n}\n\n.r-segmented-control > ul > li:last-of-type {\n    border-right: 1px solid;\n    border-top-right-radius: 4px;\n    border-bottom-right-radius: 4px;\n}\n\n.r-segmented-control > ul > li[aria-disabled=\"true\"] {\n    border-color: #ccc !important;\n    color: #ccc !important;\n    cursor: not-allowed;\n}\n\n.r-segmented-control > ul > li.base {\n    border-color: rgb(0,122,255) !important;\n    color: rgb(0,122,255);\n    background: #fff !important;\n}\n\n.r-segmented-control > ul > li.dark {\n    border-color: #333 !important;\n    color: #333;\n    background: #fff !important;\n}\n\n.r-segmented-control > ul > li.light {\n    border-color: rgb(247, 247, 247) !important;\n    color: rgb(247, 247, 247);\n    background: transparent !important;\n}\n\n.r-segmented-control > ul > li.success {\n    border-color: #5cb85c !important;\n    color: #5cb85c;\n    background: #fff !important;\n}\n\n.r-segmented-control > ul > li.error {\n    border-color: #d9534f !important;\n    color: #d9534f;\n    background: #fff !important;\n}\n\n.r-segmented-control > ul > li.base.selected {\n    background: rgb(0,122,255) !important;    \n    color: #fff !important;\n}\n\n.r-segmented-control > ul > li.dark.selected {\n    background: #333 !important;\n    color: #fff !important;\n}\n\n.r-segmented-control > ul > li.light.selected {\n    background: rgb(247, 247, 247) !important;\n    color: #333 !important;\n}\n\n.r-segmented-control > ul > li.success.selected {\n    background: #5cb85c !important; \n    color: #fff !important;\n}\n\n.r-segmented-control > ul > li.error.selected {\n    background: #d9534f !important;\n    color: #fff !important;\n}\n\n.rsc-warning {\n\n}";
 styleInject(css);
 
 var classCallCheck = function (instance, Constructor) {
@@ -89,65 +89,85 @@ var possibleConstructorReturn = function (self, call) {
 var SegmentedControl = function (_Component) {
     inherits(SegmentedControl, _Component);
 
-    function SegmentedControl() {
-        var _ref;
-
-        var _temp, _this, _ret;
-
+    function SegmentedControl(props) {
         classCallCheck(this, SegmentedControl);
 
-        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-            args[_key] = arguments[_key];
-        }
+        var _this = possibleConstructorReturn(this, (SegmentedControl.__proto__ || Object.getPrototypeOf(SegmentedControl)).call(this, props));
 
-        return _ret = (_temp = (_this = possibleConstructorReturn(this, (_ref = SegmentedControl.__proto__ || Object.getPrototypeOf(SegmentedControl)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
-            selectedSegment: 0
-        }, _this.onChange = function (selectedSegment) {
+        _this.onChange = function (selectedSegment) {
             _this.setState({ selectedSegment: selectedSegment });
             _this.props.onChangeSegment(selectedSegment);
-        }, _temp), possibleConstructorReturn(_this, _ret);
+        };
+
+        _this.renderSegments = function () {
+            return _this.props.segments.map(function (segment, i) {
+                if (i === _this.state.selectedSegment) {
+                    return React__default.createElement(
+                        'li',
+                        {
+                            key: i,
+                            className: _this.props.variant + ' selected'
+                        },
+                        segment.name
+                    );
+                } else {
+                    if (segment.disabled) {
+                        return React__default.createElement(
+                            'li',
+                            {
+                                key: i,
+                                className: '' + _this.props.variant,
+                                'aria-disabled': 'true'
+                            },
+                            segment.name
+                        );
+                    } else {
+                        return React__default.createElement(
+                            'li',
+                            {
+                                key: i,
+                                className: '' + _this.props.variant,
+                                onClick: function onClick() {
+                                    return _this.onChange(i);
+                                }
+                            },
+                            segment.name
+                        );
+                    }
+                }
+            });
+        };
+
+        _this.state = {
+            selectedSegment: 0
+        };
+        _this.renderSegments = _this.renderSegments.bind(_this);
+        return _this;
     }
 
     createClass(SegmentedControl, [{
         key: 'componentDidMount',
         value: function componentDidMount() {
-            this.setState({ selectedSegment: this.props.selected });
+            var selected = this.props.selected;
+            if (this.props.segments.map(function (e) {
+                return e.disabled;
+            }).indexOf(true) == selected) {
+                delete this.props.segments[selected].disabled;
+                console.error('Selected segment cannot be disabled');
+            }
+
+            this.setState({ selectedSegment: selected });
         }
     }, {
         key: 'render',
         value: function render() {
-            var _this2 = this;
-
             return React__default.createElement(
                 'div',
                 { className: 'r-segmented-control' },
                 React__default.createElement(
                     'ul',
                     null,
-                    this.props.segments.map(function (segment, i) {
-                        if (i === _this2.state.selectedSegment) {
-                            return React__default.createElement(
-                                'li',
-                                {
-                                    key: i,
-                                    className: _this2.props.variant + ' selected'
-                                },
-                                segment
-                            );
-                        } else {
-                            return React__default.createElement(
-                                'li',
-                                {
-                                    key: i,
-                                    className: '' + _this2.props.variant,
-                                    onClick: function onClick() {
-                                        return _this2.onChange(i);
-                                    }
-                                },
-                                segment
-                            );
-                        }
-                    })
+                    this.renderSegments()
                 )
             );
         }
--- src/components/segmented-control-react/segmented-control-react.js
+++ src/components/segmented-control-react/segmented-control-react.js
@@ -3,49 +3,78 @@ import PropTypes from 'prop-types';
 import './segmented-control-react.css';
 
 export default class SegmentedControl extends Component {
-    state = {
-        selectedSegment: 0
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedSegment: 0,
+        };
+        this.renderSegments = this.renderSegments.bind(this);
+    }
     static defaultProps = {
         onChangeSegment: x => x,
         variant: 'base',
         selected: 0
     };
     componentDidMount() {
-        this.setState({ selectedSegment: this.props.selected });        
-    }   
+        let selected = this.props.selected;
+        if(this.props.segments.map(function(e) { return e.disabled; }).indexOf(true) == selected) {
+            delete this.props.segments[selected].disabled;
+            console.error('Selected segment cannot be disabled');
+        }
+
+        this.setState({ selectedSegment: selected });
+    }
 
     onChange = selectedSegment => {
         this.setState({ selectedSegment });
         this.props.onChangeSegment(selectedSegment);
     };
+
+    renderSegments = () => {
+        return (
+            this.props.segments.map((segment, i) => {
+                if (i === this.state.selectedSegment) {
+                    return (
+                        <li
+                            key={i}
+                            className={`${this.props.variant} selected`}
+                        >
+                            {segment.name}
+                        </li>
+                    );
+                } else {
+                    if(segment.disabled) {
+                        return (
+                            <li
+                                key={i}
+                                className={`${this.props.variant}`}
+                                aria-disabled="true"
+                            >
+                                {segment.name}
+                            </li>
+                        );
+                    } else {
+                        return(
+                            <li
+                                key={i}
+                                className={`${this.props.variant}`}
+                                onClick={() => this.onChange(i)}
+                            >
+                                {segment.name}
+                            </li>
+                        );
+                    }
+                }
+            })
+        )
+    }
+
     render() {
         return (
             <div className="r-segmented-control">
                 <ul>
-                    {                                                
-                        this.props.segments.map((segment, i) => {                         
-                            if (i === this.state.selectedSegment) {
-                                return (
-                                    <li 
-                                        key={i} 
-                                        className={`${this.props.variant} selected`}
-                                    >
-                                        {segment}
-                                    </li>
-                                );
-                            } else {
-                                return (
-                                    <li
-                                        key={i}
-                                        className={`${this.props.variant}`}
-                                        onClick={() => this.onChange(i)}
-                                    >
-                                        {segment}
-                                    </li>
-                                );
-                            }
-                        })
+                    {
+                        this.renderSegments()
                     }
                 </ul>
             </div>
